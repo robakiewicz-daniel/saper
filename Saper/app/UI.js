dd0ed486a559da3ceef658b2361524fa6f3219e5
Daniel Robakiewicz <78951656+robakiewicz-daniel@users.noreply.github.com>
2022-07-07 14:26:48 +0200

    Add files via upload

---
+++ Saper/app/UI.js
@@ -0,0 +1,22 @@
+export class UI {
+  UiSelectors = {
+    board: '[data-board]',
+    cell: '[data-cell]',
+    counter: '[data-counter]',
+    timer: '[data-timer]',
+    resetButton: '[data-button-reset]',
+    easyButton: '[data-button-easy]',
+    normalButton: '[data-button-normal]',
+    expertButton: '[data-button-expert]',
+    modal: '[data-modal]',
+    modalHeader: '[data-modal-header]',
+    modalButton: '[data-modal-button]',
+  };
+
+  getElement(selector) {
+    return document.querySelector(selector);
+  }
+  getElements(selector) {
+    return document.querySelectorAll(selector);
+  }
+}
